2c07bf67002395324cffe6b302c1d6feeed7a735
Stanislav Korolkov <stasevich741@gmail.com>
2023-06-05 15:51:36 +0300

    add class

---
+++ src/test/java/github/SelenideRepositorySearchTest.java
@@ -0,0 +1,13 @@
+package github;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class SelenideRepositorySearchTest {
+
+    @Test
+    @DisplayName("Should find selenium repository at the top")
+    void shouldFindSeleniumRepositoryAtTheTop() {
+
+    }
+}
