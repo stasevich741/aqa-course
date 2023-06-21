daef4cfc55ef26ad87b4c9351aecfb18292a6dcd
Stanislav Korolkov <stasevich741@gmail.com>
2023-06-21 14:08:24 +0300

    add download test

---
+++ src/test/java/files/SelenideFilesTest.java
@@ -0,0 +1,28 @@
+package files;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class SelenideFilesTest {
+    @Test
+    void selenideDownloadTest() throws IOException {
+        open("https://github.com/assertj/assertj/blob/main/README.md");
+        File downloadedFile = $("#raw-url").download();
+
+        byte[] bytes;
+        try (InputStream is = new FileInputStream(downloadedFile)) {
+            bytes = is.readAllBytes();
+        }
+        String textContext = new String(bytes, StandardCharsets.UTF_8);
+        assertThat(textContext).contains("AssertJ - Fluent Assertions");
+    }
+}
