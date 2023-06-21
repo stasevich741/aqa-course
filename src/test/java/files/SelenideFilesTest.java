5cc070002d752442e3f42597fd22c3abbb40964e
Stanislav Korolkov <stasevich741@gmail.com>
2023-06-21 14:34:52 +0300

    add upload test

--- src/test/java/files/SelenideFilesTest.java
+++ src/test/java/files/SelenideFilesTest.java
@@ -1,5 +1,6 @@
 package files;
 
+import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
@@ -8,11 +9,15 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class SelenideFilesTest {
+    public SelenideElement inputFile = $("input[qq-button-id='a76480db-11c2-4f48-91bf-a0539376ce20']");
+
+    public SelenideElement inputFile2 = $x("//input[contains(@qq-button-id, 'db')]");
+
+
     @Test
     void selenideDownloadTest() throws IOException {
         open("https://github.com/assertj/assertj/blob/main/README.md");
@@ -24,5 +29,11 @@ public class SelenideFilesTest {
         }
         String textContext = new String(bytes, StandardCharsets.UTF_8);
         assertThat(textContext).contains("AssertJ - Fluent Assertions");
+    }
+
+    @Test
+    void selenideUploadTest() {
+        open("https://fineuploader.com/demos");
+        $("input[type='file']").uploadFromClasspath("img/1.png");
     }
 }
